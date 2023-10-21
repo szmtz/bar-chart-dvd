ce39cb1f0cad3c7c8e57a766a906b8242f5ed589
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-21 19:24:47 +0000

    Set up margin dimensions of the chart

---
+++ script.js
@@ -0,0 +1,6 @@
+//set up the dimensions of the chart
+const margin = {top: 70, right: 40, bottom: 60, left:175}
+const width = 660 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
+
+
