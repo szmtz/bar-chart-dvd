8b0105d29862edb53a97676feff6004f0ac3ffd7
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-21 19:28:06 +0000

    Create SVG container for chart

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ const margin = {top: 70, right: 40, bottom: 60, left:175}
 const width = 660 - margin.left - margin.right
 const height = 400 - margin.top - margin.bottom
 
+// create the SVG container for the chart 
+const svg = d3.select("body-chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+
 
